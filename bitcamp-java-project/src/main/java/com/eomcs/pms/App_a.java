14b13ae0f0167857f1273e3c56aab7f41298dd39
hayeon17kim <qwndhs@gmail.com>
2020-07-31 16:19:20 +0900

    java-project-05 클래스와 인스턴스

--- bitcamp-java-project/src/main/java/com/eomcs/pms/App_a.java
+++ bitcamp-java-project/src/main/java/com/eomcs/pms/App_a.java
@@ -1,179 +1,90 @@
 package com.eomcs.pms;
 
-// 클래스의 패키지 정보를 미리 선언하여 컴파일러에게 알려준다.
-// ex) Scanner는 java.util에 있는 Scanner라는 것을 먼저 알려주
+
 import java.util.Scanner;
 import java.sql.Date;
 
-// 1) 낱개의 변수를 사용하여 여러 회원 정보 처리하기 
+// 1) 회원의 번호, 이름, 이메일, 암호, 사진, 전화번호, 등록일을 담을 수 있는
+//    메모리를 설계한다. => Member 클래스 정의
+// 2) 
 public class App_a {
 
   public static void main(String[] args) {
     
-
+    // 값을 담기 위해 메모리를 준비할 때
+    // 어떤 종류의 메모리를 준비해야 하는 지 설계도를 작성한다.
+    // => 이것을 "클래스 정의" 라고 부른다. 
+    class Member {
+      int no;
+      String name;
+      String email;
+      String password;
+      String photo;
+      String tel;
+      Date now;
+    }
+    
+    
     Scanner keyInput = new Scanner(System.in);
 
-    
     System.out.println("[회원]");
     
-    // 최대 5명의 회원 정보를 입력 받는 변수를 선언
-    int no1, no2, no3, no4, no5;
-    String name1, name2, name3, name4, name5;
-    String email1, email2, email3, email4, email5;
-    String password1, password2, password3, password4, password5;
-    String photo1, photo2, photo3, photo4, photo5;
-    String tel1, tel2, tel3, tel4, tel5;
-    java.sql.Date now1, now2, now3, now4, now5;
-    long currentMillis;
+    final int MAX_LENGTH = 5;
+    int[] no = new int[MAX_LENGTH];
+    String[] name = new String[MAX_LENGTH];
+    String[] email = new String[MAX_LENGTH];
+    String[] password = new String[MAX_LENGTH];
+    String[] photo = new String[MAX_LENGTH];
+    String[] tel = new String[MAX_LENGTH];
+    Date[] now = new Date[MAX_LENGTH];
+    long currentMillis = 0;
+    int count = 0;
     
-    // 1번 회원 입력 
-    System.out.print("번호? ");
-    no1 = keyInput.nextInt();
-    keyInput.nextLine();     
+    //MAX_LENGTH = 100;
     
-    System.out.print("이름? ");
-    name1 = keyInput.nextLine();
+    for (int i = 0; i < MAX_LENGTH; i++) {
+      count ++;
+      System.out.print("번호? ");
+      no[i] = keyInput.nextInt();
+      keyInput.nextLine();     
+      
+      System.out.print("이름? ");
+      name[i] = keyInput.nextLine();
+      
+      System.out.print("이메일? ");
+      email[i] = keyInput.nextLine();
+      
+      System.out.print("암호? ");
+      password[i] = keyInput.nextLine();
+      
+      System.out.print("사진? ");
+      photo[i] = keyInput.nextLine();
+      
+      System.out.print("전화? ");
+      tel[i] = keyInput.nextLine();
+      
+      currentMillis = System.currentTimeMillis();
+      now[i] = new Date(currentMillis);
+      
+      System.out.println("");
+      
+      System.out.println("계속 입력하시겠습니까?(y/N) ");
+      
     
-    System.out.print("이메일? ");
-    email1 = keyInput.nextLine();
-    
-    System.out.print("암호? ");
-    password1 = keyInput.nextLine();
-    
-    System.out.print("사진? ");
-    photo1 = keyInput.nextLine();
-    
-    System.out.print("전화? ");
-    tel1 = keyInput.nextLine();
-    
-    currentMillis = System.currentTimeMillis();
-    now1 = new Date(currentMillis);
-    
-    System.out.println("");
-    
-    
-    //2번 회원 입력 
-    System.out.print("번호? ");
-    no2 = keyInput.nextInt();
-    keyInput.nextLine();     
-    
-    System.out.print("이름? ");
-    name2 = keyInput.nextLine();
-    
-    System.out.print("이메일? ");
-    email2 = keyInput.nextLine();
-    
-    System.out.print("암호? ");
-    password2 = keyInput.nextLine();
-    
-    System.out.print("사진? ");
-    photo2 = keyInput.nextLine();
-    
-    System.out.print("전화? ");
-    tel2 = keyInput.nextLine();
-    
-    currentMillis = System.currentTimeMillis();
-    now2 = new Date(currentMillis);
-    
-    System.out.println("");
-    
-    
-    //3번 회원 입력 
-    
-    System.out.print("번호? ");
-    no3 = keyInput.nextInt();
-    keyInput.nextLine();     
-    
-    System.out.print("이름? ");
-    name3 = keyInput.nextLine();
-    
-    System.out.print("이메일? ");
-    email3 = keyInput.nextLine();
-    
-    System.out.print("암호? ");
-    password3 = keyInput.nextLine();
-    
-    System.out.print("사진? ");
-    photo3 = keyInput.nextLine();
-    
-    System.out.print("전화? ");
-    tel3 = keyInput.nextLine();
-    
-    currentMillis = System.currentTimeMillis();
-    now3 = new Date(currentMillis);
-    
-    System.out.println("");
-    
-    
-    //4번 회원 입력 
-    System.out.print("번호? ");
-    no4 = keyInput.nextInt();
-    keyInput.nextLine();     
-    
-    System.out.print("이름? ");
-    name4 = keyInput.nextLine();
-    
-    System.out.print("이메일? ");
-    email4 = keyInput.nextLine();
-    
-    System.out.print("암호? ");
-    password4 = keyInput.nextLine();
-    
-    System.out.print("사진? ");
-    photo4 = keyInput.nextLine();
-    
-    System.out.print("전화? ");
-    tel4 = keyInput.nextLine();
-    
-    currentMillis = System.currentTimeMillis();
-    now4 = new Date(currentMillis);
-    
-    System.out.println("");
-    
-    
-    // 5번 회원 입력
-    System.out.print("번호? ");
-    no5 = keyInput.nextInt();
-    keyInput.nextLine();     
-    
-    System.out.print("이름? ");
-    name5 = keyInput.nextLine();
-    
-    System.out.print("이메일? ");
-    email5 = keyInput.nextLine();
-    
-    System.out.print("암호? ");
-    password5 = keyInput.nextLine();
-    
-    System.out.print("사진? ");
-    photo5 = keyInput.nextLine();
-    
-    System.out.print("전화? ");
-    tel5 = keyInput.nextLine();
-    
-    currentMillis = System.currentTimeMillis();
-    now5 = new Date(currentMillis);
-    
-    System.out.println("");
-    
+      String response = keyInput.nextLine();
+      if (response.equalsIgnoreCase("y") == false) {
+        break;  // 반복문을 멈춰라.
+      }
+    }
 
-    
-//    System.out.print("계속 입력하시겠습니까?(y/N) ");
-//    String response = keyInput.nextLine();
-//    
-//    if (response.equals("y")) {
-//      //추가로 회원 정보를 입력 받는다.
-//      
-//    }
-//    
     keyInput.close();
     
     System.out.println("--------------------------");
-    System.out.printf("%d, %s, %s, %s, %s\n", no1, name1, email1, tel1, now1.toString());
-    System.out.printf("%d, %s, %s, %s, %s\n", no2, name2, email2, tel2, now2.toString());
-    System.out.printf("%d, %s, %s, %s, %s\n", no3, name3, email3, tel3, now3.toString());
-    System.out.printf("%d, %s, %s, %s, %s\n", no4, name4, email4, tel4, now4.toString());
-    System.out.printf("%d, %s, %s, %s, %s\n", no5, name5, email5, tel5, now5.toString());
-
+    
+    for (int i = 0; i < count; i++) {
+      System.out.printf("%d, %s, %s, %s, %s\n", 
+          no[i], name[i], email[i], tel[i], now[i].toString());
+    }
+    
   }
 }
