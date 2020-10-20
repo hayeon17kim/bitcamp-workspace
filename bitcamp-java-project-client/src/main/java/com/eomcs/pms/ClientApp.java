812df3ad05f1406f630b0db16bf0f7a9ec2c6f49
hayeon17kim <qwndhs@gmail.com>
2020-10-20 19:40:42 +0900

    thread pool

--- bitcamp-java-project-client/src/main/java/com/eomcs/pms/ClientApp.java
+++ bitcamp-java-project-client/src/main/java/com/eomcs/pms/ClientApp.java
@@ -48,25 +48,34 @@ public class ClientApp {
   }
   
   private static void request(String message) {
+    // 클라이언트가 서버에 stop 명령을 보내면 다음 변수를 true로 변경한다.
+    boolean stop = false;
+
     try (Socket socket = new Socket(host, port);
-        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        PrintWriter out = new PrintWriter(socket.getOutputStream())) {
+        PrintWriter out = new PrintWriter(socket.getOutputStream());
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+
       out.println(message);
       out.flush();
+
       receiveResponse(out, in);
-      
+
+      if (message.equalsIgnoreCase("stop")) {
+        stop = true;
+      }
     } catch (Exception e) {
-      
+      e.printStackTrace();
     }
-    
+
     if (stop) {
+      // 서버를 멈추기 위해 그냥 접속했다가 끊는다.
       try (Socket socket = new Socket(host, port)) {
-        
+        // 아무것도 안한다.
+        // 서버가 stop 할 기회를 주기 위함이다.
       } catch (Exception e) {
-        
+        // 아무것도 안한다.
       }
     }
-    
   }
   
   
--- bitcamp-java-project-server/src/main/java/com/eomcs/pms/ServerApp.java
+++ bitcamp-java-project-server/src/main/java/com/eomcs/pms/ServerApp.java
@@ -7,9 +7,12 @@ import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import com.eomcs.context.ApplicationContextListener;
 import com.eomcs.pms.handler.Command;
 import com.eomcs.pms.listener.AppInitListener;
@@ -17,30 +20,38 @@ import com.eomcs.pms.listener.DataHandlerListener;
 import com.eomcs.pms.listener.RequestMappingListener;
 
 public class ServerApp {
-  
-  // 클라이언트가 stop명령을 보내면 이 값이 true로 변경된다.
-  // 인스턴스 메서드가 되면 안된다. handleClient가 static 이기 때문에  static 멤버에만 접근 가능하다.
+
+  // 클라이언트가 "stop" 명령을 보내면 이 값이 true로 변경된다.
+  // - 이 값이 true 이면 다음 클라이언트 접속할 때 서버를 종료한다.
   static boolean stop = false;
 
-  static Map<String, Object> context = new HashMap<>();
+  // 스레드풀 준비
+  ExecutorService threadPool = Executors.newCachedThreadPool();
 
+  // 옵저버와 공유할 맵 객체
+  static Map<String,Object> context = new Hashtable<>();
+
+  // 옵저버를 보관할 컬렉션 객체
   List<ApplicationContextListener> listeners = new ArrayList<>();
 
-
-  private void addApplicationContextListener (ApplicationContextListener listener) {
+  // 옵저버를 등록하는 메서드
+  public void addApplicationContextListener(ApplicationContextListener listener) {
     listeners.add(listener);
   }
 
-  private void removeApplicationContextListener (ApplicationContextListener listener) {
+  // 옵저버를 제거하는 메서드
+  public void removeApplicationContextListener(ApplicationContextListener listener) {
     listeners.remove(listener);
   }
 
+  // 옵저버에게 통지한다.
   private void notifyApplicationContextListenerOnServiceStarted() {
     for (ApplicationContextListener listener : listeners) {
       listener.contextInitialized(context);
     }
   }
 
+  // 옵저버에게 통지한다.
   private void notifyApplicationContextListenerOnServiceStopped() {
     for (ApplicationContextListener listener : listeners) {
       listener.contextDestroyed(context);
@@ -48,66 +59,95 @@ public class ServerApp {
   }
 
   public void service(int port) {
-    
+
     notifyApplicationContextListenerOnServiceStarted();
-    
-    try (ServerSocket ss = new ServerSocket(8888)){
+
+    try (ServerSocket serverSocket = new ServerSocket(port)) {
       System.out.println("서버 실행 중...");
+
       while (true) {
-        Socket clientSocket = ss.accept();
-        
-        new Thread(() ->handleClient(clientSocket)).start();
+        Socket clientSocket = serverSocket.accept();
+
         if (stop) {
-          break; // 즉시 멈추진 않고, 다음의 클라이언트가 접속할 때 안되겠지.
+          break;
         }
+        // 직접 스레드를 생성하는 것이 아니라 스레드풀에 작업을 맡긴다.
+        threadPool.execute(() -> handleClient(clientSocket));
       }
+
     } catch (Exception e) {
       e.printStackTrace();
     }
+
     notifyApplicationContextListenerOnServiceStopped();
+    threadPool.shutdown();
+    
+    try {
+      if (!threadPool.awaitTermination(10, TimeUnit.SECONDS)) {
+        System.out.println("아직 종료 안 된 작업이 있다.");
+        System.out.println("남아 있는 작업의 강제 종료를 시도하겠다.");
+        threadPool.shutdownNow();
+        
+        if (!threadPool.awaitTermination(5, TimeUnit.SECONDS)) {
+          System.out.println("스레드풀의 강제 종료를 완료하지 못했다.");
+        } else {
+          System.out.println("모든 작업을 강제 종료했다.");
+        }
+      }
+    } catch (Exception e) {
+      System.out.println("스레드풀 종료 중 오류 발생!");
+    }
+    System.out.println("서버 종료!");
   }
 
   public static void main(String[] args) {
     ServerApp server = new ServerApp();
-    
-    // 리스너(옵저버 /구독자) 등록 
+
+    // 리스너(옵저버/구독자) 등록
     server.addApplicationContextListener(new AppInitListener());
     server.addApplicationContextListener(new DataHandlerListener());
     server.addApplicationContextListener(new RequestMappingListener());
-    
+
     server.service(8888);
   }
 
   private static void handleClient(Socket clientSocket) {
     InetAddress address = clientSocket.getInetAddress();
-    System.out.printf("클라이언트(%s)가 연결되었습니다.\n", address.getHostAddress());
-    try (Socket socket = clientSocket; // try 블록에서 떠날 때 close()가 자동 호출된다.
+    System.out.printf("클라이언트(%s)가 연결되었습니다.\n",
+        address.getHostAddress());
+
+    try (Socket socket = clientSocket; // try 블록을 떠날 때 close()가 자동 호출된다.
         BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        PrintWriter out = new PrintWriter(socket.getOutputStream()))  {
-        String request = in.readLine();
-        if (request.equalsIgnoreCase("stop")) {
-          stop= true; // 서버의 상태를 멈추라는 의미로 true로 설정한다.
-          out.println("서버를 종료하는 중입니다.");
-          out.println();
-          out.flush();
-          return;
-        }
-        
-        Command command = (Command) context.get(request);
-        if (command != null) {
-          // 출력 스트림을 파라미터로 넘긴다.
-          command.execute(out, in);
-        } else {
-          out.println("해당 명령을 처리할 수 없습니다!");
-        }
-        // 응답의 끝을 알리는 빈 문자열을 보낸다.
+        PrintWriter out = new PrintWriter(socket.getOutputStream())) {
+
+      // 클라이언트가 보낸 요청을 읽는다.
+      String request = in.readLine();
+
+      if (request.equalsIgnoreCase("stop")) {
+        stop = true; // 서버의 상태를 멈추라는 의미로 true로 설정한다.
+        out.println("서버를 종료하는 중입니다!");
         out.println();
         out.flush();
+        return;
+      }
+
+      Command command = (Command) context.get(request);
+      if (command != null) {
+        command.execute(out, in);
+      } else {
+        out.println("해당 명령을 처리할 수 없습니다!");
+      }
+
+      // 응답의 끝을 알리는 빈 문자열을 보낸다.
+      out.println();
+      out.flush();
 
     } catch (Exception e) {
+      System.out.println("클라이언트와의 통신 오류!");
     }
-    
-    System.out.printf("클라이언트(%s)와의 연결을 끊습니다.\n", address.getHostAddress());
+
+    System.out.printf("클라이언트(%s)와의 연결을 끊었습니다.\n",
+        address.getHostAddress());
   }
 }
 
